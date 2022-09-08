ce891466e400869f0e3e354952ec7bca34ca05d3
Anass Saidi <anas.15.saidi@gmail.com>
2022-09-08 15:20:37 +0100

    Handling the API reponses and filtering the data

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -30,7 +31,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -82,11 +85,11 @@ public class MainActivity extends AppCompatActivity {
         cityName = getCityNAme(location.getLongitude(),location.getLatitude());
         getWeatherInfo(cityName);
 
-        searchIV.setOnClickListener(new View.OnClickListener() {
+        searchIV.setOnClickListener(new View.OnClickListener() { // On Click on the Search Icon
             @Override
             public void onClick(View v) {
-                String city = cityEdt.getText().toString();
-                if(city.isEmpty()){
+                String city = cityEdt.getText().toString(); //Getting the Text
+                if(city.isEmpty()){ //checking for empty entry
                     Toast.makeText(MainActivity.this, "Please Enter city name", Toast.LENGTH_SHORT).show();
                 }else{
                     cityNameTV.setText(cityName);
@@ -97,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) { //Handling the permissions
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode==PERMISSION_CODE){
             if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
@@ -109,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String getCityNAme(double longitude, double latitude){
+    private String getCityNAme(double longitude, double latitude){ //Getting the city name from longitude & latitude
         String cityName = "Not found";
         Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
         try {
@@ -132,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
         return cityName;
     }
 
-    private void getWeatherInfo(String cityName){
+    private void getWeatherInfo(String cityName){ //Getting the weather informations
         String url = "http://api.weatherapi.com/v1/forecast.json?key=ca3f05ebbc6a46efb32153855220609&q=" + cityName + "&days=1&aqi=yes&alerts=yes";
         cityNameTV.setText(cityName);
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
@@ -146,6 +149,35 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String temperature = response.getJSONObject("current").getString("temp_c");
                     temperatureTV.setText(temperature + "°C");
+                    int isDay = response.getJSONObject("current").getInt("is_day");
+                    String condition = response.getJSONObject("current").getJSONObject("condition").getString("text");
+                    String conditionIcon = response.getJSONObject("current").getJSONObject("condition").getString("icon");
+                    Picasso.get().load("http:".concat(conditionIcon)).into(iconIV);
+                    conditionTV.setText(condition);
+                    if(isDay==1){
+                        //morning
+                        Picasso.get().load("https://images.unsplash.com/photo-1522441815192-d9f04eb0615c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80").into(backIV);
+                    }else{
+                        //night
+                        Picasso.get().load("https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80").into(backIV);
+                    }
+
+                    JSONObject forecastObj = response.getJSONObject("forecast");
+                    JSONObject forecast0 = forecastObj.getJSONArray("forecastday").getJSONObject(0);
+                    JSONArray hourArray = forecast0.getJSONArray("hour");
+
+                    for(int i=0; i<hourArray.length(); i++){
+                        JSONObject hourObj = hourArray.getJSONObject(i);
+                        String time = hourObj.getString("time");
+                        String temper = hourObj.getString("temp_c");
+                        String img = hourObj.getJSONObject("condition").getString("icon");
+                        String windSpeed = hourObj.getString("wind_kph");
+
+                        weatherRVModelArrayList.add(new WeatherRVModel(time, temper, img, windSpeed));
+                    }
+
+                    weatherRVAdapter.notifyDataSetChanged();
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
